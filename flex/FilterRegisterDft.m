ad54d17efb11e33e115290f534c8f30ab2e12824
Nathan Perkins <nathan@maxmo.net>
2017-09-21 16:43:22 -0400

    Allow specifying registration frame
    Useful when aligning across videos.

--- flex/FilterRegisterDft.m
+++ flex/FilterRegisterDft.m
@@ -7,17 +7,32 @@ classdef FilterRegisterDft < Filter
     end
     
     properties (Access=protected)
+        reuse_ref = false;
         ref;
     end
     
     methods
+        function FF = FilterRegisterDft(initial_ref)
+            FF@Filter();
+            
+            % seed initial reference frame
+            if exist('initial_ref', 'var') && ~isempty(initial_ref)
+                if ~isa(initial_ref, 'double') && ~isa(initial_ref, 'single')
+                    initial_ref = im2single(initial_ref);
+                end
+                
+                FF.ref = fft2(initial_ref);
+                FF.reuse_ref = true;
+            end
+        end
+        
         function frame = processFrame(FF, frame, i)
             % convert from integer
             if ~isa(frame, 'double') && ~isa(frame, 'single')
-                frame = single(frame);
+                frame = im2single(frame);
             end
             
-            if i == 1
+            if i == 1 && ~FF.reuse_ref
                 % store reference
                 FF.ref = fft2(frame);
                 
--- flex/FilterRegisterMatlab.m
+++ flex/FilterRegisterMatlab.m
@@ -6,18 +6,35 @@ classdef FilterRegisterMatlab < Filter
         opt;
         metric;
         
+        reuse_ref = false;
         ref;
     end
     
     methods
+        function FF = FilterRegisterMatlab(initial_ref)
+            FF@Filter();
+            
+            % seed initial reference frame
+            if exist('initial_ref', 'var') && ~isempty(initial_ref)
+                if ~isa(initial_ref, 'double') && ~isa(initial_ref, 'single')
+                    initial_ref = single(initial_ref);
+                end
+                
+                FF.ref = fft2(initial_ref);
+                FF.reuse_ref = true;
+            end
+        end
+        
+        function setup(FF, video_details, dim_in)
+            % configure matlab registration
+            [FF.opt, FF.metric] = imregconfig('monomodal');
+            FF.opt.MaximumIterations = 300;
+        end
+        
         function frame = processFrame(FF, frame, i)
-            if i == 1
+            if i == 1 && ~FF.reuse_ref
                 % store reference
                 FF.ref = frame;
-                    
-                % configure matlab registration
-                [FF.opt, FF.metric] = imregconfig('monomodal');
-                FF.opt.MaximumIterations = 300;
                 
                 return;
             end
