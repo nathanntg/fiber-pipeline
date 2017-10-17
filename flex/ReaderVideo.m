3ac53c7548cf53e607f33ecb8b23f37a332327b7
Nathan Perkins <nathan@maxmo.net>
2017-10-17 12:37:17 -0400

    Allow reading subset of frames

--- flex/ReaderVideo.m
+++ flex/ReaderVideo.m
@@ -3,21 +3,37 @@ classdef ReaderVideo < Reader
     %   Detailed explanation goes here
     
     properties
-        fh;
         video;
-        last_frame;
+        
+        read_start;
+        read_frames;
+    end
+    
+    properties (Access=protected)
+        fh;
         frame_number = 0;
         read_size;
         read_precision;
+        
+        cur_start;
+        cur_frames;
     end
     
     methods
-        function FR = ReaderVideo(video)
+        function FR = ReaderVideo(video, start, frames)
             % call parent constructor
             FR@Reader();
             
             % store video file
             FR.video = video;
+            
+            % range
+            if exist('start', 'var') && ~isempty(start)
+                FR.read_start = start;
+            end
+            if exist('frames', 'var') && ~isempty(frames)
+                FR.read_frames = frames;
+            end
         end
         
         function [details, dim_out, type_out] = getVideoDetails(FR)
@@ -42,8 +58,25 @@ classdef ReaderVideo < Reader
                 'depth', depth, 'bytes', bytes ...
             );
         
+            % frames to read
+            if isempty(FR.read_start) || FR.read_start == 0
+                FR.cur_start = 1;
+            elseif FR.read_start < 0
+                FR.cur_start = frames + FR.read_start;
+                if FR.cur_start < 1
+                    error('Unable to read the desired number of frames.');
+                end
+            else
+                FR.cur_start = FR.read_start;
+            end
+            if isempty(FR.read_frames)
+                FR.cur_frames = frames + 1 - FR.cur_start;
+            else
+                FR.cur_frames = min(frames + 1 - FR.cur_start, FR.read_frames);
+            end
+        
             % output types
-            dim_out = [height width frames];
+            dim_out = [height width FR.cur_frames];
             type_out = sprintf('uint%d', 8 * ceil(depth / 8));
         end
         
@@ -60,6 +93,11 @@ classdef ReaderVideo < Reader
             
             % reset frame number
             FR.frame_number = 0;
+            
+            % jump to start
+            if FR.cur_start > 1
+                fseek(FR.fh, prod(FR.read_size) * ceil(video_details.depth / 8) * (FR.cur_start - 1), 'bof');
+            end
             
             % call parent
             setup@Reader(FR, video_details, dim_in, type_in);
